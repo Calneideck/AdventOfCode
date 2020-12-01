84cab691d03d07dc23f903014fecb08c6cc3ce85
Callum Neideck <cal_neideck@hotmail.com>
2020-12-01 16:44:17 +1100

    Added 2020 day 1

---
+++ 2020/Day.cs
@@ -0,0 +1,15 @@
+﻿namespace AdventOfCode
+{
+    public class Day
+    {
+        public virtual string Part1()
+        {
+            return "";
+        }
+
+        public virtual string Part2()
+        {
+            return "";
+        }
+    }
+}
---
+++ 2020/Days/Day1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    class Day1 : Day
+    {
+        string[] lines = File.ReadAllLines("Input/1.txt");
+
+        public override string Part1()
+        {
+            IEnumerable<int> numbers = lines.Select(line => int.Parse(line));
+            foreach (int num in numbers)
+                foreach (int num2 in numbers)
+                    if (num + num2 == 2020)
+                        return (num * num2).ToString();
+
+            return "";
+        }
+
+        public override string Part2()
+        {
+            IEnumerable<int> numbers = lines.Select(line => int.Parse(line));
+            foreach (int num in numbers)
+                foreach (int num2 in numbers)
+                    foreach (int num3 in numbers)
+                        if (num + num2 + num3 == 2020)
+                            return (num * num2 * num3).ToString();
+
+            return "";
+        }
+    }
+}
