0f8ee7d7a98ad43eedc7160e19f15d8d129690eb
Callum Neideck <cal_neideck@hotmail.com>
2020-12-21 17:24:26 +1100

    Day 18 and Day 21

---
+++ 2020/Days/Day18.cs
@@ -0,0 +1,115 @@
+﻿using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode
+{
+    class Day18 : Day
+    {
+        string[] lines = File.ReadAllLines("Input/18.txt").ToArray();
+
+        public override object Part1()
+        {
+            long sum = 0;
+            Stack<long> sums = new Stack<long>();
+            Stack<char> ops = new Stack<char>();
+            foreach (var line in lines)
+            {
+                long currentSum = 0;
+                char currentOp = ' ';
+
+                for (int i = 0; i < line.Length; i++)
+                {
+                    if (line[i] == ' ')
+                        continue;
+                    else if (line[i] == '+')
+                        currentOp = '+';
+                    else if (line[i] == '*')
+                        currentOp = '*';
+                    else if (line[i] == '(')
+                    {
+                        sums.Push(currentSum);
+                        ops.Push(currentOp);
+                        currentSum = 0;
+                        currentOp = ' ';
+                    }
+                    else if (line[i] == ')')
+                    {
+                        currentOp = ops.Pop();
+                        if (currentOp == ' ')
+                            sums.Pop();
+                        else if (currentOp == '+')
+                            currentSum += sums.Pop();
+                        else if (currentOp == '*')
+                            currentSum *= sums.Pop();
+
+                        currentOp = ' ';
+                    }
+                    else if (currentOp == '*')
+                    {
+                        currentSum *= int.Parse(line[i].ToString());
+                        currentOp = ' ';
+                    }
+                    else if (currentOp == '+')
+                    {
+                        currentSum += int.Parse(line[i].ToString());
+                        currentOp = ' ';
+                    }
+                    else
+                        currentSum = int.Parse(line[i].ToString());
+                }
+
+                sum += currentSum;
+            }
+
+            return sum;
+        }
+
+        public override object Part2()
+        {
+            for (int i = 0; i < lines.Length; i++)
+                for (int x = 0; x < lines[i].Length; x++)
+                    if (lines[i][x] == '+')
+                    {
+                        int y = x - 1;
+                        int slashes = 0;
+                        while (true)
+                        {
+                            y--;
+                            if (int.TryParse(lines[i][y].ToString(), out _))
+                            {
+                                if (slashes == 0)
+                                    break;
+                            }
+                            else if (lines[i][y] == ')')
+                                slashes++;
+                            else if (lines[i][y] == '(' && --slashes == 0)
+                                break;
+                        }
+
+                        lines[i] = lines[i].Insert(y, "(");
+                        y = x + 1;
+
+                        while (true)
+                        {
+                            y++;
+                            if (int.TryParse(lines[i][y].ToString(), out _))
+                            {
+                                if (slashes == 0)
+                                    break;
+                            }
+                            else if (lines[i][y] == '(')
+                                slashes++;
+                            else if (lines[i][y] == ')' && --slashes == 0)
+                                break;
+                        }
+
+                        lines[i] = lines[i].Insert(y + 1, ")");
+                        x += 4;
+                    }
+
+            return Part1();
+        }
+    }
+}
---
+++ 2020/Days/Day21.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode
+{
+    class Day21 : Day
+    {
+        string[] lines = File.ReadAllLines("Input/21.txt").ToArray();
+        Dictionary<string, string> allergens = new Dictionary<string, string>();
+
+        public override object Part1()
+        {
+            Dictionary<string, List<List<string>>> allergenPossibleIngredients = new Dictionary<string, List<List<string>>>();
+            List<string> allIngredients = new List<string>();
+            Regex reg = new Regex(@"[a-z]+");
+
+            foreach (var line in lines)
+            {
+                var words = reg.Matches(line);
+                bool gettingIngredients = true;
+
+                List<string> ingredients = new List<string>();
+
+                foreach (var word in words)
+                    if (word.ToString() == "contains")
+                        gettingIngredients = false;
+                    else if (gettingIngredients)
+                    {
+                        ingredients.Add(word.ToString());
+                        allIngredients.Add(word.ToString());
+                    }
+                    else
+                    {
+                        allergenPossibleIngredients.TryAdd(word.ToString(), new List<List<string>>());
+                        allergenPossibleIngredients[word.ToString()].Add(ingredients);
+                    }
+            }
+
+            while (allergens.Keys.Count != allergenPossibleIngredients.Keys.Count)
+                foreach (var allergen in allergenPossibleIngredients)
+                {
+                    var allergenIngredients = allergen.Value.Aggregate((list, other) => list.Intersect(other).ToList()).Except(allergens.Values);
+                    if (allergenIngredients.Count() == 1)
+                        allergens.Add(allergen.Key, allergenIngredients.First());
+                }
+
+            return allIngredients.Count(i => !allergens.Values.Contains(i));
+        }
+
+        public override object Part2()
+        {
+            var _allergens = allergens.ToList();
+            _allergens.Sort((a, b) => a.Key.CompareTo(b.Key));
+            return string.Join(',', _allergens.Select(a => a.Value));
+        }
+    }
+}
