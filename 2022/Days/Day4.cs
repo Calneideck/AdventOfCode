0f21556a2895f3fb2cf8eb3d53198b816c499570
Callum Neideck <cal_neideck@hotmail.com>
2022-12-05 19:36:28 +1100

    Added day 4

---
+++ 2022/Days/Day4.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    class Day4 : Day
+    {
+        string[] lines = File.ReadAllLines("Input/4.txt");
+
+        public override object Part1()
+        {
+            int count = 0;
+
+            foreach (var line in lines)
+            {
+                int[][] a = line.Split(',').Select(x => x.Split('-').Select(int.Parse).ToArray()).ToArray();
+
+                for (int i = 0; i < 2; i++)
+                    if (a[i][0] >= a[1 - i][0] && a[i][1] <= a[1 - i][1])
+                    {
+                        count++;
+                        break;
+                    }
+            }
+
+            return count;
+        }
+
+        public override object Part2()
+        {
+            int count = 0;
+
+            foreach (var line in lines)
+            {
+                int[][] a = line.Split(',').Select(x => x.Split('-').Select(int.Parse).ToArray()).ToArray();
+
+                for (int i = 0; i < 2; i++)
+                    if ((a[i][0] >= a[1 - i][0] && a[i][0] <= a[1 - i][1]) || 
+                        (a[i][1] >= a[1 - i][0] && a[i][1] <= a[1 - i][1]))
+                    {
+                        count++;
+                        break;
+                    }
+            }
+
+            return count;
+        }
+    }
+}
