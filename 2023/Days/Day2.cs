d2c3df21b1fa53bc752a2d20783496af71717502
Callum Neideck <cal_neideck@hotmail.com>
2023-12-03 22:47:54 +1100

    Day 2 & 3

---
+++ 2023/Days/Day2.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    class Day2 : Day
+    {
+        string[] lines = File.ReadAllLines("Input/2.txt");
+        int r = 12;
+        int g = 13;
+        int b = 14;
+
+        public override object Part1()
+        {
+            int sum = 0;
+
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(": ");
+
+                int gameId = int.Parse(parts[0].Split(" ")[1]);
+                IEnumerable<string[]> parts2 = parts[1].Split("; ").Select(a => a.Split(", "));
+
+                bool allGood = true;
+
+                foreach (string[] draw in parts2)
+                    foreach (string cubes in draw)
+                    {
+                        string[] cube = cubes.Split(" ");
+                        int count = int.Parse(cube[0].ToString());
+                        string colour = cube[1];
+
+                        if (count > GetColourMax(colour))
+                        {
+                            allGood = false;
+                            break;
+                        }
+                    }
+
+                if (allGood) sum += gameId;
+            }
+
+            return sum;
+        }
+
+        public override object Part2()
+        {
+            int sum = 0;
+
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(": ");
+
+                IEnumerable<string[]> parts2 = parts[1].Split("; ").Select(a => a.Split(", "));
+
+                Dictionary<string, int> maxCounts = new()
+                {
+                    { "red", 0 },
+                    { "green", 0 },
+                    { "blue", 0 }
+                };
+
+                foreach (string[] draw in parts2)
+                    foreach (string cubes in draw)
+                    {
+                        string[] cube = cubes.Split(" ");
+                        int count = int.Parse(cube[0].ToString());
+                        string colour = cube[1];
+
+                        maxCounts[colour] = Math.Max(maxCounts[colour], count);
+                    }
+
+                sum += maxCounts.Values.Aggregate(1, (sum, x) => sum * x);
+            }
+
+            return sum;
+        }
+
+        int GetColourMax(string input)
+        {
+            return input switch
+            {
+                "red" => r,
+                "green" => g,
+                "blue" => b,
+                _ => 0,
+            };
+        }
+    }
+}
---
+++ 2023/Days/Day3.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    class Day3 : Day
+    {
+        string[] lines = File.ReadAllLines("Input/3.txt");
+
+        Dictionary<V2, string> map = new();
+
+        public override object Part1()
+        {
+            int sum = 0;
+
+            for (int y = 0; y < lines.Length; y++)
+                for (int x = 0; x < lines[y].Length; x++)
+                    map.Add(new V2(x, y), lines[y][x].ToString());
+
+
+            for (int y = 0; y < lines.Length; y++)
+            {
+                bool parsingNumber = false;
+                string num = "";
+
+                for (int x = 0; x < lines[y].Length; x++)
+                {
+                    bool isNum = int.TryParse(lines[y][x].ToString(), out int i);
+                    if (isNum)
+                    {
+                        if (!parsingNumber)
+                            parsingNumber = true;
+
+                        num += i.ToString();
+                    }
+
+                    if (parsingNumber && (!isNum || x == lines[0].Length - 1))
+                    {
+                        int number = int.Parse(num);
+                        bool symbol = false;
+
+                        for (int j = x - num.Length - 1; j <= x; j++)
+                            for (int k = y - 1; k <= y + 1; k++)
+                            {
+                                V2 v = new(j, k);
+                                if (map.ContainsKey(v) && map[v] != "." && !int.TryParse(map[v], out int _))
+                                {
+                                    symbol = true;
+                                    break;
+                                }
+                            }
+
+                        if (symbol)
+                            sum += number;
+
+                        num = "";
+                        parsingNumber = false;
+                    }
+                }
+            }
+
+            return sum;
+        }
+
+        public override object Part2()
+        {
+            Dictionary<V2, List<int>> powers = new();
+
+            for (int y = 0; y < lines.Length; y++)
+            {
+                bool parsingNum = false;
+                string num = "";
+
+                for (int x = 0; x < lines[y].Length; x++)
+                {
+                    bool isNum = int.TryParse(lines[y][x].ToString(), out int i);
+                    if (isNum)
+                    {
+                        if (!parsingNum)
+                            parsingNum = true;
+
+                        num += i.ToString();
+                    }
+
+                    if (parsingNum && (!isNum || x == lines[0].Length - 1))
+                    {
+                        int number = int.Parse(num);
+
+                        for (int j = x - num.Length - 1; j <= x; j++)
+                            for (int k = y - 1; k <= y + 1; k++)
+                            {
+                                V2 v = new(j, k);
+                                if (map.ContainsKey(v) && map[v] == "*")
+                                {
+                                    if (powers.ContainsKey(v))
+                                        powers[v].Add(number);
+                                    else
+                                        powers.Add(v, new() { number });
+                                }
+                            }
+
+                        num = "";
+                        parsingNum = false;
+                    }
+                }
+            }
+
+            return powers.Values
+                .Where(x => x.Count == 2)
+                .Select((a) => a.Aggregate(1, (b, c) => b * c))
+                .Sum();
+        }
+    }
+}
