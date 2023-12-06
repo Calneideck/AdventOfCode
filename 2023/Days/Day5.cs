5d30fb3017d4750663e537d8ba19027d0a4f7db0
Callum Neideck <cal_neideck@hotmail.com>
2023-12-06 18:48:46 +1100

    Day 5 part 1

---
+++ 2023/Days/Day5.cs
@@ -0,0 +1,91 @@
+﻿using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+
+namespace AdventOfCode
+{
+    class Day5 : Day
+    {
+        string[] lines = File.ReadAllLines("Input/5.txt");
+        long[] seeds = null;
+        List<List<long[]>> map = new();
+
+        public override object Part1()
+        {
+            foreach (var line in lines)
+            {
+                if (line.StartsWith("seeds"))
+                {
+                    seeds = new Regex(@"(\d+)")
+                        .Matches(line).Select(a => long.Parse(a.Value))
+                        .ToArray();
+                }
+                else if (line.Contains("-to-"))
+                    map.Add(new List<long[]>());
+                else if (line.Length > 1)
+                {
+                    // numbers
+                    long[] nums = new Regex(@"(\d+)")
+                        .Matches(line).Select(a => long.Parse(a.Value))
+                        .ToArray();
+                    map.Last().Add(new long[] { nums[0], nums[1], nums[2] });
+                }
+            }
+
+            return seeds.Min((seed) => FindMin(seed, 1));
+        }
+
+        public override object Part2()
+        {
+            Task[] tasks = new Task[seeds.Length / 2];
+            ConcurrentBag<long> bag = new();
+
+            for (int i = 0; i < seeds.Length; i += 2)
+            {
+                int index = i;
+                tasks[index / 2] = Task.Factory.StartNew(() =>
+                {
+                    bag.Add(FindMin(seeds[index], seeds[index + 1]));
+                });
+            }
+
+            Task.WaitAll([.. tasks]);
+
+            return bag.Min();
+        }
+
+        long FindMin(long start, long length)
+        {
+            long min = long.MaxValue;
+            long end = start + length;
+
+            for (long x = start; x < end; x++)
+            {
+                long seed = x;
+
+                int mapLen = map.Count;
+                for (int i = 0; i < mapLen; i++)
+                {
+                    int map2Len = map[i].Count;
+                    for (int j = 0; j < map2Len; j++)
+                    {
+                        long s = map[i][j][1];
+                        if (seed >= s && seed <= s + map[i][j][2])
+                        {
+                            seed = map[i][j][0] + (seed - s);
+                            break;
+                        }
+                    }
+                }
+
+                min = Math.Min(min, seed);
+            }
+
+            return min;
+        }
+    }
+}
