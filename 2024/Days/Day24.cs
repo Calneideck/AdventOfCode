eb86a5996e407534c7a7139d0fb3b155b26e2229
Callum Neideck <cal_neideck@hotmail.com>
2025-12-03 21:23:23 +1100

    Day 3 and 2024-24 apparently?

--- 2024/Days/Day24.cs
+++ 2024/Days/Day24.cs
@@ -9,10 +9,63 @@ namespace AdventOfCode
     {
         string[] lines = File.ReadAllLines("Input/24.txt");
 
+        class Gate
+        {
+            public string[] inputs;
+            public int op;
+            public string output;
+        }
+
         public override object Part1()
         {
-            int sum = 0;
+            long sum = 0;
+            bool top = true;
+            Dictionary<string, bool> map = new();
+            List<Gate> gates = new();
+            HashSet<Gate> seen = new();
 
+            foreach (string s in lines)
+                if (s.Length == 0)
+                    top = false;
+                else if (top)
+                {
+                    string[] a = s.Split(": ");
+                    map.Add(a[0], a[1] == "1");
+                }
+                else
+                {
+                    string[] a = s.Split(" ");
+                    string[] inps = new string[] { a[0], a[2] };
+                    int op = 0;
+                    if (a[1] == "OR") op = 1;
+                    else if (a[1] == "XOR") op = 2;
+
+                    gates.Add(new Gate() { inputs = inps, op = op, output = a[4] });
+                }
+
+            while (seen.Count != gates.Count)
+                foreach (Gate g in gates)
+                    if (
+                        !seen.Contains(g) &&
+                        map.TryGetValue(g.inputs[0], out bool left) &&
+                        map.TryGetValue(g.inputs[1], out bool right)
+                    )
+                    {
+                        if (g.op == 0)
+                            map[g.output] = left & right;
+                        else if (g.op == 1)
+                            map[g.output] = left | right;
+                        else if (g.op == 2)
+                            map[g.output] = left ^ right;
+
+                        seen.Add(g);
+                    }
+
+            foreach (var kv in map.Where(kv => kv.Key.StartsWith("z") && kv.Value))
+            {
+                int i = int.Parse(kv.Key[1..]);
+                sum += (long)Math.Pow(2, i);
+            }
 
             return sum;
         }
---
+++ 2025/Days/Day3.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    class Day3 : Day
+    {
+        string[] lines = File.ReadAllLines("Input/3.txt");
+
+        public override object Part1()
+        {
+            int count = 0;
+
+            foreach (string line in lines)
+            {
+                int[] nums = [.. line.ToCharArray().Select(c => int.Parse(c.ToString()))];
+
+                int max = nums[0..^1].Max();
+                int index = nums.IndexOf(max) + 1;
+                int max2 = nums[index..].Max();
+
+                count += int.Parse(max + "" + max2);
+            }
+
+            return count;
+        }
+
+        public override object Part2()
+        {
+            ulong count = 0;
+
+            foreach (string line in lines)
+            {
+                int[] nums = [.. line.ToCharArray().Select(c => int.Parse(c.ToString()))];
+
+                int index = 0;
+                string num = "";
+
+                for (int i = 11; i >= 0; i--)
+                {
+                    int max = nums[index..^i].Max();
+                    index += nums[index..^i].IndexOf(max) + 1;
+                    num += max;
+                }
+
+                count += ulong.Parse(num);
+            }
+
+            return count;
+        }
+    }
+}
